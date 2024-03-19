ffc46f819dcb70bd5d9b8c1cacc8780fa708295a
JACKYZHOUHZ <1255674591@qq.com>
2024-03-20 01:05:25 +0800

    Exo 2

---
+++ src/callExceptionMethods/Main.java
@@ -0,0 +1,19 @@
+package callExceptionMethods;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            int result = divide(10, 0);
+            System.out.println("结果: " + result);
+        } catch (ArithmeticException e) {
+            System.out.println("捕获到算术异常：" + e.getMessage()); // 显示错误信息
+        }
+    }
+
+    public static int divide(int dividend, int divisor) {
+        if (divisor == 0) {
+            throw new ArithmeticException("不能除以零");
+        }
+        return dividend / divisor;
+    }
+}
