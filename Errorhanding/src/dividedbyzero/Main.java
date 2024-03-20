d85bdd72b2a360fc02ff7f82889630e52097658c
Alex9597 <18367107406@163.com>
2024-03-20 15:51:05 +0800

    NSS

--- Errorhanding/src/dividedbyzero/Main.java
+++ Errorhanding/src/dividedbyzero/Main.java
@@ -4,7 +4,11 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println(3/0);
+		try {
+            System.out.println(3 / 0);
+        } catch (ArithmeticException e) {
+            System.out.printf("捕获的运行时异常：%s\n", e.getMessage());
+        }
 	}
 
 }
